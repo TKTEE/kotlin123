a5492d9591537c5f444d495c8c0cc28ad3514f07
consolatamburu <consolatamburu@gmail.com>
2023-04-20 15:04:08 +0300

    my commit

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var city:String = "Nairobi"
+    var car = "GT3-RS"
+    var age:Int = 20
+    var domain:Char = 'T'
+    var status:Boolean = true
+    var temperature:Float = 34.5F
+    var results:Double = 23.212
+}
